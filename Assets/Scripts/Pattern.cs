e678cc2c30a4482d9683d44219c266c86394d5df
Matthew Mitchell <matt.mancer@gmail.com>
2015-09-13 13:58:00 +0100

    Add menus and sounds

---
+++ Assets/Scripts/Pattern.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Pattern {
+    public LinkedList<PatternNode> pattern = new LinkedList<PatternNode>();
+    public LinkedList<PatternNode> playerAttempt = new LinkedList<PatternNode>();
+
+    public bool successful = false;
+
+    public void ReplacePatternNodeSet(LinkedList<PatternNode> nodes) {
+        for (var node = pattern.First; node != null; node = node.Next) {
+            foreach (var newNode in nodes) {
+                if (newNode.number == node.Value.number) {
+                    pattern.AddBefore(node, newNode);
+                    pattern.Remove(node);
+                    node = pattern.Find(newNode);
+                }
+            }
+        }
+    }
+
+    public void ReplacePlayerNodeSet(LinkedList<PatternNode> nodes) {
+        for (var node = playerAttempt.First; node != null; node = node.Next) {
+            foreach (var newNode in nodes) {
+                if (newNode.number == node.Value.number) {
+                    playerAttempt.AddBefore(node, newNode);
+                    playerAttempt.Remove(node);
+                    node = playerAttempt.Find(newNode);
+                }
+            }
+        }
+    }
+
+    public void DisaplyPattern() {
+        for (var node = pattern.First; node.Next != null; node = node.Next) {
+            node.Value.lineRenderer.SetPosition(1, node.Next.Value.transform.position - node.Value.transform.position);
+        }
+    }
+
+    public void DisaplyPlayerPattern() {
+        if (playerAttempt.Count > 0) {
+            for (var node = playerAttempt.First; node.Next != null; node = node.Next) {
+                node.Value.lineRenderer.SetPosition(1, node.Next.Value.transform.position - node.Value.transform.position);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ScoreDisplay : MonoBehaviour {
+    [SerializeField] private GameObject display1_;
+    [SerializeField] private GameObject display2_;
+    [SerializeField] private GameObject tick;
+    [SerializeField] private GameObject cross;
+
+    private LinkedList<PatternNode> nodes1_ = new LinkedList<PatternNode>();
+    private LinkedList<PatternNode> nodes2_ = new LinkedList<PatternNode>();
+
+    private void Awake() {
+        foreach (Transform child in display1_.transform) {
+            nodes1_.AddLast(child.GetComponent<PatternNode>());
+        }
+
+        foreach (Transform child in display2_.transform) {
+            nodes2_.AddLast(child.GetComponent<PatternNode>());
+        }
+    }
+
+    public void Initiate(Pattern pattern, float xOffset) {
+        pattern.ReplacePlayerNodeSet(nodes1_);
+        pattern.ReplacePatternNodeSet(nodes2_);
+
+        pattern.DisaplyPattern();
+        pattern.DisaplyPlayerPattern();
+
+        transform.Translate(-Vector3.up * xOffset);
+
+        if (pattern.successful) {
+            tick.SetActive(true);
+        } else {
+            cross.SetActive(true);
+        }
+    }
+}
